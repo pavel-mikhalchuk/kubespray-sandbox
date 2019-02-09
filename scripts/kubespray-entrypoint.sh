1b66038d904f70368fc14d8e9828630c34e8db28
Pavel Mikhalchuk <pavel.mikhalchuk@gmail.com>
2019-02-09 06:37:42 +0000

    docker files; kubespray docker Dentrypoint script; ansible master playbook

---
+++ scripts/kubespray-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+useradd -m ${USER}
+
+mkdir -p /home/${USER}/.ssh/
+
+cp /host_ssh/id_rsa /home/${USER}/.ssh/id_rsa
+chown ${USER}:${USER} /home/${USER}/.ssh/id_rsa
+cp /host_ssh/id_rsa.pub /home/${USER}/.ssh/id_rsa.pub
+chown ${USER}:${USER} /home/${USER}/.ssh/id_rsa.pub
+
+su ${USER} -c '"$0" "$@"' -- "$@" 
