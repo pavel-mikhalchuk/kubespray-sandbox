00801c1a782ef7e221cd3e2b8300f9c48a4435e4
Pavel Mikhalchuk <pavel.mikhalchuk.consultant@nielsen.com>
2019-02-16 06:33:54 +0300

    make /kubespray folder belong to kubespray user

--- scripts/kubespray-entrypoint.sh
+++ scripts/kubespray-entrypoint.sh
@@ -8,5 +8,6 @@ cp /host_ssh/id_rsa /home/${USER}/.ssh/id_rsa
 chown ${USER}:${USER} /home/${USER}/.ssh/id_rsa
 cp /host_ssh/id_rsa.pub /home/${USER}/.ssh/id_rsa.pub
 chown ${USER}:${USER} /home/${USER}/.ssh/id_rsa.pub
+chown -R ${USER}:${USER} /kubespray
 
 su ${USER} -c '"$0" "$@"' -- "$@" 
