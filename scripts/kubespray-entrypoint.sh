155ca0d79488268b3cb12ee5ee6ec76a7b955468
Pavel Mikhalchuk <pavel.mikhalchuk.consultant@nielsen.com>
2019-02-19 07:54:37 +0300

    move "kube config copy from external docker volume" to entrypoint script

--- scripts/kubespray-entrypoint.sh
+++ scripts/kubespray-entrypoint.sh
@@ -4,12 +4,23 @@ useradd -m ${USER}
 
 echo "$USER ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers
 
+# SSH keys
 mkdir -p /home/${USER}/.ssh/
 
 cp /host_ssh/id_rsa /home/${USER}/.ssh/id_rsa
 chown ${USER}:${USER} /home/${USER}/.ssh/id_rsa
 cp /host_ssh/id_rsa.pub /home/${USER}/.ssh/id_rsa.pub
 chown ${USER}:${USER} /home/${USER}/.ssh/id_rsa.pub
+# SSH keys
+
+# Kube config
+mkdir -p /home/${USER}/.kube/
+
+cp /kube_config/config /home/${USER}/.kube/config &> /dev/null
+chown ${USER}:${USER} /home/${USER}/.kube/config &> /dev/null
+# Kube config
+
+# Get permissions for kubespray folder so ansible .retry file can be written 
 chown -R ${USER}:${USER} /kubespray
 
 su ${USER} -c '"$0" "$@"' -- "$@" 
