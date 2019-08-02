229622707d14cb8a714aa6c8783a5c788348906a
Pavel Mikhalchuk <pavel.mikhalchuk@gmail.com>
2019-08-03 01:58:29 +0200

    docker for kube-logging ansible; more gigs for for sentry pv

---
+++ kube-logging/cluster-bootstrap/ansible/ssh-utils/keys/copy-ssh-keys.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USER=""
+
+for i in "$@"
+do
+case $i in
+    -u=*|--user=*)
+    USER="${i#*=}"
+    shift # past argument=value
+    ;;
+    -p=*|--password=*)
+    NODE_PASSWORD="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+      echo "UKNOWN" # unknown option
+    ;;
+esac
+done
+
+if [[ -z "$USER" ]]; then
+    echo "User name is not specified"
+    exit 1
+fi
+
+if [[ -z "$NODE_PASSWORD" ]]; then
+    echo "Password is not specified"
+    exit 1
+fi
+
+echo "User: $USER"
+echo "Pswd: $NODE_PASSWORD"
+
+mkdir ~/.ssh
+mkdir /generated_ssh_key
+
+echo -e 'y\n' | ssh-keygen -b 2048 -t rsa -f "/generated_ssh_key/${USER}_rsa" -q -N ""
+echo "Keys are generated"
+echo "Now copying keys to nodes..."
+
+for ip in `cat ssh-utils/keys/hosts_where_to_copy_ssh_keys`; do
+    sshpass -p ${NODE_PASSWORD} ssh-copy-id -o StrictHostKeyChecking=no -i "/generated_ssh_key/${USER}_rsa.pub" ${USER}@$ip
+done
